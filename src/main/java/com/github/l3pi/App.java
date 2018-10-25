d2765b96529ee3de69d55346617d9b39b637ff31
tao <zhaopeng.tao@etu.unice.fr>
2018-10-25 23:11:45 +0200

    added main for the game

--- src/main/java/com/github/l3pi/App.java
+++ src/main/java/com/github/l3pi/App.java
@@ -1,5 +1,11 @@
 package com.github.l3pi;
 
+import com.github.l3pi.game.GameManager;
+import com.github.l3pi.game.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -9,6 +15,17 @@ public class App
     public static void main( String[] args ) {
         if (args.length != 2) {
             System.err.printf("Usage: %s <run-count> <player-count(>=2)>", args[0]);
+
+        }
+        else{
+            List<Player> players = new ArrayList<Player>();
+            players.add(new Player(3,"player1"));
+            players.add(new Player(2,"player2"));
+            GameManager gameManager = new GameManager(players);
+            gameManager.run();
+
+        }
+
         }
     }
 }
