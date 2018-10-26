afb886fb0953e2354afb18c3451a0e3d17abd0fb
tao <zhaopeng.tao@etu.unice.fr>
2018-10-26 10:43:32 +0200

    added test for facetRuleManager,playerTest,RuleFactoruTest

--- src/test/java/com/github/l3pi/game/FacetRuleManagerTest.java
+++ src/test/java/com/github/l3pi/game/FacetRuleManagerTest.java
@@ -1,17 +1,28 @@
 package com.github.l3pi.game;
 
+import com.github.l3pi.type.FacetType;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class FacetRuleManagerTest {
+    private FacetRuleManager facetRuleManager;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
+        facetRuleManager = new FacetRuleManager(
+            RuleFactory.getInstance().getRules(0)
+        );
     }
 
     @Test
     public void resolve() {
+        List<Operation> ops = facetRuleManager.resolve(Collections.singletonList(new Facet(FacetType.GOLD, 1)));
+
+        assertTrue(ops.size() > 0);
     }
 }
--- src/test/java/com/github/l3pi/game/RuleFactoryTest.java
+++ src/test/java/com/github/l3pi/game/RuleFactoryTest.java
@@ -1,17 +1,32 @@
 package com.github.l3pi.game;
 
+import com.github.l3pi.rule.FacetRule;
+import com.github.l3pi.rule.RuleSet;
+import com.github.l3pi.type.FacetType;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Set;
 
 import static org.junit.Assert.*;
 
 public class RuleFactoryTest {
+    private RuleSet set;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
+        set = RuleFactory.getInstance().getRules(0);
     }
 
     @Test
     public void getRules() {
+        HashMap<FacetType, FacetRule> facetRules = set.facetRule;
+        Set<FacetType> facets = facetRules.keySet();
+
+        assertTrue(facets.contains(FacetType.GOLD));
+        assertTrue(facets.contains(FacetType.GLORY));
+        assertTrue(facets.contains(FacetType.SOLAR));
+        assertTrue(facets.contains(FacetType.LUNAR));
     }
 }
