9f5502981d7929fa3bbfa6d075737e394ffb69ac
Rohydre <sushi.soumrany@gmail.com>
2018-11-08 17:38:32 +0100

    Inventory update, forge and face history

--- src/main/java/com/github/l3pi/game/Inventory.java
+++ src/main/java/com/github/l3pi/game/Inventory.java
@@ -9,15 +9,21 @@ public class Inventory {
     private final HashMap<ResourceType, Integer> resources;
     private final Random random;
     private final Facet[] faceUp;
+    private final List<Facet> faceHistory;
+    private final Player player;
+    private final Game game;
 
-    public Inventory(Facet[][] dices, HashMap<ResourceType, Integer> resources) {
+    public Inventory(Game game, Player player, Facet[][] dices, HashMap<ResourceType, Integer> resources) {
         this.dices = dices;
         this.resources = resources;
         this.random = new Random();
         this.faceUp = new Facet[dices.length];
+        this.faceHistory = new ArrayList<Facet>();
+        this.player = player;
+        this.game = game;
     }
 
-    public List<Facet> throwDice() {
+    public List<Facet> throwDice() {  // == Faveur Majeur
         List<Facet> facets = new ArrayList<Facet>();
         for (int i = 0 ; i < dices.length; i++) {
             int randIndex = random.nextInt(dices[i].length);
@@ -26,6 +32,18 @@ public class Inventory {
         }
         return facets;
     }
+    public void forge(Facet facetToForge){
+        if (facetToForge != null) {
+            int[] choosenIndex = player.forgeMyDice(game);
+            int choosenDice = choosenIndex[0];
+            int choosenFacet = choosenIndex[1];
+            faceHistory.add(dices[choosenDice][choosenFacet]);
+            dices[choosenDice][choosenFacet] = facetToForge;
+            faceUp[choosenDice] = dices[choosenDice][choosenFacet];
+        }
+    }
+
+
 
     public Facet[] getFaceUp(){
         return Arrays.copyOf(faceUp,faceUp.length);
