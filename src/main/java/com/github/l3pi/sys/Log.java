750f8a9f20c6418c48776eb50c1a18d346f7cad2
Artemix <artemix@artemix.org>
2018-12-06 14:45:10 +0100

    Added colour support to logger

--- src/main/java/com/github/l3pi/sys/Log.java
+++ src/main/java/com/github/l3pi/sys/Log.java
@@ -1,8 +1,41 @@
 package com.github.l3pi.sys;
 
 public class Log {
+    private static class Colour {
+        static final String RESET = "\u001B[0m";
+        static final String BLACK = "\u001B[30m";
+        static final String RED = "\u001B[31m";
+        static final String GREEN = "\u001B[32m";
+        static final String YELLOW = "\u001B[33m";
+        static final String BLUE = "\u001B[34m";
+        static final String PURPLE = "\u001B[35m";
+        static final String CYAN = "\u001B[36m";
+        static final String WHITE = "\u001B[37m";
+
+        static String fmt(String colour, String msg) {
+            return colour + msg + RESET;
+        }
+    }
+
+    public enum State { INFO, ACTION, STATUS }
+
     private static boolean IS_ENABLED = false;
     public static void enableLog() { IS_ENABLED = true; }
-    public static void log(String m) { if (IS_ENABLED) { System.out.println(m); } }
+    public static void log(State s, String m) {
+        String colour = Colour.WHITE;
+
+        switch (s) {
+            case ACTION:
+                colour = Colour.GREEN;
+                break;
+            case STATUS:
+                colour = Colour.BLUE;
+        }
+
+        if (IS_ENABLED) {
+            System.out.println(Colour.fmt(colour, m));
+        }
+    }
+    public static void log(String m) { log(State.INFO, m); }
     public static void log() { log(""); }
 }
