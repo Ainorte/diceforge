528484f2144ab1903221e31ab2e8ace44e22db3e
tao <zhaopeng.tao@etu.unice.fr>
2018-12-15 16:07:57 +0100

    test #47

--- src/test/java/com/github/l3pi/bot/RandomBotTest.java
+++ src/test/java/com/github/l3pi/bot/RandomBotTest.java
@@ -8,90 +8,82 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.github.l3pi.game.Factory.generateDices;
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertSame;
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class RandomBotTest {
 
     private Player bot;
+    private List<Facet> dices;
+    private List<Card> cards;
     private Game game;
     @Before
     public void setUp(){
+        this.bot = new RandomBot(1);
+        this.dices = Factory.generateDiceSanctuary(new ArrayList<Player>() {{
+            add(bot);
+        }}).getAvailableFacets();
+        this.cards = Factory.generateCardSanctuary(new ArrayList<Player>() {{
+            add(bot);
+        }}).getAvailableCard();
+        this.game = mock(Game.class);
 
-        this.bot = new RandomBot("playerbot");
-        List<Player> p = new ArrayList<Player>();
-        p.add(this.bot);
-        p.add(new RandomBot("playerbot2"));
-        this.game = new Game(p);
+        DiceSanctuary diceSanctuary = mock(DiceSanctuary.class);
+        Inventory inventory = mock(Inventory.class);
+        CardSanctuary cardSanctuary = mock(CardSanctuary.class);
+
+        when(game.getDiceSanctuary()).thenReturn(diceSanctuary);
+        when(diceSanctuary.getAvailableFacets()).thenReturn(dices);
+        when(game.getInventory(bot)).thenReturn(inventory);
+        when(inventory.getDices()).thenReturn(generateDices());
+        when(game.getCardSanctuary()).thenReturn(cardSanctuary);
+        when(cardSanctuary.getAvailableCard()).thenReturn(cards);
+        when(cardSanctuary.getPurchasableCard(any())).thenReturn(cards);
     }
 
     @Test
-    public void testMethods(){
-        assertTrue(this.bot.chooseFacetToForge(this.game.getDiceSanctuary().getPurchasableInventory(this.game.getInventory(this.bot).getResource(ResourceType.GOLD)),this.game) instanceof Facet);
-        assertTrue((this.bot.chooseCard(this.game) instanceof Card)||(this.bot.chooseCard(this.game)==null));
+    public void chooseFacetToForge() {
+        Facet result = bot.chooseFacetToForge(dices, game);
+        assertTrue(result == null || dices.stream().anyMatch(facet -> facet == result));
+    }
 
-        List<Integer> t = new ArrayList<Integer>(){};
-        t.add(0);
-        t.add(1);
-        t.add(2);
-        assertTrue((t.contains(this.bot.chooseAction(this.game))));
-        t.add(6);
-        ArrayList dice1 = new ArrayList<Facet>() {{
-            add(new Facet("1 GOLD",
-                ((Game game, Player player) -> {
-                    game.getInventory(player).addResources(ResourceType.GOLD, 1);
-                })));
-            add(new Facet("1 GOLD",
-                ((Game game, Player player) -> {
-                    game.getInventory(player).addResources(ResourceType.GOLD, 1);
-                })));
-            add(new Facet("1 GOLD",
-                ((Game game, Player player) -> {
-                    game.getInventory(player).addResources(ResourceType.GOLD, 1);
-                })));
-            add(new Facet("1 GOLD",
-                ((Game game, Player player) -> {
-                    game.getInventory(player).addResources(ResourceType.GOLD, 1);
-                })));
-            add(new Facet("1 GOLD",
-                ((Game game, Player player) -> {
-                    game.getInventory(player).addResources(ResourceType.GOLD, 1);
-                })));
-            add(new Facet("1 SOLAR",
-                ((Game game, Player player) -> {
-                    game.getInventory(player).addResources(ResourceType.SOLAR, 1);
-                })));
-        }};
+    @Test
+    public void chooseCard() {
+        Card result = bot.chooseCard(game);
+        assertTrue(result == null || cards.stream().anyMatch(card -> card == result));
+    }
 
-        ArrayList dice2 = new ArrayList<Facet>() {{
-            add(new Facet("1 GOLD",
-                ((Game game, Player player) -> {
-                    game.getInventory(player).addResources(ResourceType.GOLD, 1);
-                })));
-            add(new Facet("1 GOLD",
-                ((Game game, Player player) -> {
-                    game.getInventory(player).addResources(ResourceType.GOLD, 1);
-                })));
-            add(new Facet("1 GOLD",
-                ((Game game, Player player) -> {
-                    game.getInventory(player).addResources(ResourceType.GOLD, 1);
-                })));
-            add(new Facet("1 GOLD",
-                ((Game game, Player player) -> {
-                    game.getInventory(player).addResources(ResourceType.GOLD, 1);
-                })));
-            add(new Facet("1 LUNAR",
-                ((Game game, Player player) -> {
-                    game.getInventory(player).addResources(ResourceType.LUNAR, 1);
-                })));
-            add(new Facet("2 GLORY",
-                ((Game game, Player player) -> {
-                    game.getInventory(player).addResources(ResourceType.GLORY, 2);
-                })));}};
-            List<Dice> dices = new ArrayList<Dice>(){{
-                add(new Dice(dice1));
-                add(new Dice(dice2));
-            }};
-        assertTrue((t.contains(this.bot.chooseDice(dices))));
+    @Test
+    public void chooseAction() {
+        assertTrue(bot.chooseAction(game) == 0 || bot.chooseAction(game) == 1);
+    }
+
+    @Test
+    public void chooseResource() {
+        List<ResourceType> ressources = new ArrayList<>();
+        ressources.add(ResourceType.LUNAR);
+        assertSame(bot.chooseResource(ressources), ResourceType.LUNAR);
+    }
+
+    @Test
+    public void chooseDice() {
+        List<Dice> dices = generateDices();
+        int result = bot.chooseDice(dices);
+        assertTrue(result == 0 || result == 1 || result == 2);
+
+    }
+
+    @Test
+    public void forgeMyDice() {
+        Facet facet =  new Facet("1 GOLD",
+            ((Game game, Player player) -> game.getInventory(player).addGold(player, 1)));
+        assertEquals(2, bot.forgeDice(game, facet).length);
     }
 
 }
