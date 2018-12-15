697b2cd8eb8ab671a6db3d79c19226ae115a5add
William Poitevin <william.poitevin@gmail.com>
2018-12-15 16:03:41 +0100

    Refactor Game for testing and secure game End #41

--- src/main/java/com/github/l3pi/game/Game.java
+++ src/main/java/com/github/l3pi/game/Game.java
@@ -2,7 +2,6 @@ package com.github.l3pi.game;
 
 import com.github.l3pi.sys.Log;
 import com.github.l3pi.type.ResourceType;
-import com.github.l3pi.utilities.Tuple;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -16,89 +15,82 @@ public class Game {
     /**
      * la classe Game gere tout ce qui est interaction du jeu pour chaque tour, pour faire un jeu complet voir GameManager
      * qui executer le nombre de tours correspondant aux nombres de joueurs
-     *
+     * <p>
      * la variable players est une map ordonnée de la classe player qui associe son inventaire
      * la variable diceSanctuary est la variable qui représente le sanctuaire des dés
      * la variable cardSanctuary est la variable qui représente le sanctuaire des cartes
-     *
-     * */
+     */
 
     private TreeMap<Player, Inventory> players;
     private DiceSanctuary diceSanctuary;
     private CardSanctuary cardSanctuary;
-    private int actualRound; // count the round for intelligentBot
+    private int round;
+    private int maxRound;
 
-    /** On instancie Game avec une liste de joueurs
-     * @param players  a liste de joueurs pour cette partie
+    /**
+     * On instancie Game avec une liste de joueurs
      *
-     * */
-    public Game(List<Player> players) {
-        init(players);
+     * @param players a liste de joueurs pour cette partie
+     */
+    public Game(TreeMap<Player, Inventory> players, DiceSanctuary diceSanctuary, CardSanctuary cardSanctuary, int round, int maxRound) {
+        this.players = new TreeMap<>(players);
+        this.diceSanctuary = diceSanctuary;
+        this.cardSanctuary = cardSanctuary;
+        this.round = round;
+        this.maxRound = maxRound;
     }
 
-    public void init(List<Player> players) {
-        this.players = new TreeMap<>();
-        this.diceSanctuary = new DiceSanctuary();
-        this.cardSanctuary = new CardSanctuary(players);
-        this.actualRound = 0;
-        for (int i =0; i < players.size(); i++) {
-            this.players.put(players.get(i), new Inventory(3 - i));
-        }
+    public int getRound() {
+        return this.round;
     }
 
-    void increaseRound(){
-        this.actualRound++;
+    public boolean isFinish() {
+        return round >= maxRound;
     }
 
-    public int getActualRound(){
-        return this.actualRound;
-    }
-
-
-    /** applique un tour de jeu
-     * */
-
+    /**
+     * applique un tour de jeu
+     */
     void round() {
-        for (Player player : this.getPlayers()) {
-            round(player);
+        if (!isFinish()) {
+            round++;
+            log(Log.State.RED, String.format("\n======================================== Tour %d ========================================\n", round));
+            for (Player player : this.getPlayers()) {
+                round(player);
+            }
+            log(Log.State.GREEN, "Etat du jeu à la fin du tour " + getRound());
+            log();
+            log(Log.State.GREEN, toString());
+            log(Log.State.RED, String.format("\n====================================== Fin Tour %d ======================================\n", round));
         }
     }
 
-    /**Applique un tour de jeu et gere l'ordre des actions pendant un tour
-     * */
-
+    /**
+     * Applique un tour de jeu et gere l'ordre des actions pendant un tour
+     */
     private void round(Player player) {
-        log(Log.State.STATUS, "Tour de jeu de " + player);
-
-        if(getPlayers().size() == 2){
+        log(Log.State.GREEN, "Tour de jeu de " + player);
+        log();
+        if (getPlayers().size() == 2) {
             divineBlessing();
         }
         divineBlessing();
         log();
         recurrentAction(player);
+        log();
         action(player);
         log();
         log();
     }
 
-    void recurrentAction(Player player) {
-        log(Log.State.STATUS, "Application des cartes récurrentes de " + player);
-        getInventory(player)
-            .getCards().stream()
-            .filter(Card::isRecurrent)
-            .forEach(card -> {
-                card.getOperation().apply(this, player);
-            });
-    }
-
-    /** l'action qui permet de lancer le dé des joueurs l'un aprés l'autre
-     *
-     * */
-    void divineBlessing() {
-        log(Log.State.STATUS, "Faveur divine lancée pour les joueurs");
+    /**
+     * l'action qui permet de lancer le dé des joueurs l'un aprés l'autre
+     */
+    private void divineBlessing() {
+        log(Log.State.RED, "Faveur divine lancée pour tous les joueurs");
         for (Player player : getPlayers()) {
             List<Facet> facetUp = getInventory(player).throwDices();
-            log(Log.State.ACTION, player.getName() + " a lancé " + facetUp);
+            log(Log.State.GREEN, "\t" + player + Log.State.GREEN + " a lancé " + facetUp);
 
         }
         for (Player player : getPlayers()) {
@@ -108,38 +100,69 @@ public class Game {
         }
     }
 
-    /** cette fonction applique une action en fonction du choix du joueur
+    private void recurrentAction(Player player) {
+        log(Log.State.RED, "Application des cartes récurrentes de " + player);
+        getInventory(player)
+            .getCardInventory().stream()
+            .filter(Card::isRecurrent)
+            .forEach(card -> card.getOperation().apply(this, player));
+    }
+
+    /**
+     * cette fonction applique une action en fonction du choix du joueur
      * par exemple le joueur peut decider d'acheter une carte ou de forger une face de dé
+     *
      * @param player le player qui effectue l'action
      */
-    void action(Player player) {
-        log(Log.State.LOG, player + " joue");
-
-        if(player.chooseAction(this) == 0) {
-            Facet facet = player.chooseFacetToForge(this.getDiceSanctuary().getPurchasableInventory(this.getInventory(player).getResource(ResourceType.GOLD)),this);
-            if (facet != null) {
-                facet = this.diceSanctuary.buyFacet(facet);
-                log(Log.State.ACTION, player.getName() + " a acheté la face de dés " + facet + " pour " + this.diceSanctuary.getPriceForFacet(facet) + " Or");
-                int[] diceChangeFace = player.forgeMyDice(this, facet);
-                this.getInventory(player).forge(facet, diceChangeFace[0], diceChangeFace[1]);
-            }
+    private void action(Player player) {
+        log(Log.State.RED, player + Log.State.RED + " joue");
+        Inventory inventory = this.players.get(player);
+        action(player.chooseAction(this), player);
+        if (inventory.getResource(ResourceType.SOLAR) >= 2 && player.moreAction(this)) {
+            inventory.addResources(ResourceType.SOLAR, -2);
+            action(player.chooseAction(this), player);
         }
-        else{
-            final Card card = player.chooseCard(this);
-            if (card != null) {
-                this.cardSanctuary.buyCard(card);
-                Inventory inventory = this.players.get(player);
-                inventory.addCard(card);
-                card.getResourceType().forEach(resource -> {
-                    inventory.addResources(resource,-card.getPrice());
-                });
+    }
 
-                this.players.put(player,inventory);
-                if(!card.isRecurrent()){
-                    card.getOperation().apply(this,player);
+    private void action(int action, Player player) {
+        Inventory inventory = this.players.get(player);
+        if (action == 0) {
+            Facet facet;
+            do {
+                facet = player.chooseFacetToForge(this.getDiceSanctuary().getPurchasableFacets(this.getInventory(player).getResource(ResourceType.GOLD)), this);
+                facet = this.diceSanctuary.buyFacet(facet);
+                if (facet != null) {
+                    inventory.addResources(ResourceType.GOLD, -facet.getGoldCost());
+                    log(Log.State.GREEN, "\t" + player + Log.State.GREEN + " a acheté la face de dés " + facet + " pour " + facet.getGoldCost() + " gold");
+                    int[] diceChangeFace = player.forgeDice(this, facet);
+                    this.getInventory(player).forge(facet, diceChangeFace[0], diceChangeFace[1]);
                 }
-                cardSanctuary.move(card.getLocationType(), player);
-                log(Log.State.ACTION, card.displayEffect(player) + " en achetant la carte " + card +" et se situe sur la case " + card.getLocationType() + " du plateau");
+            } while (facet != null);
+        } else if (action == 1) {
+            Card card = player.chooseCard(this);
+            card = this.cardSanctuary.buyCard(card);
+            if (card != null) {
+                final int cost = card.getPrice();
+                card.getResourceType().forEach(resource -> {
+                    inventory.addResources(resource, -cost);
+                });
+                Player old = cardSanctuary.move(card.getLocation(), player);
+
+                inventory.addCard(card);
+
+                log(Log.State.GREEN, "\t" + player + Log.State.GREEN + " a acheté la carte " + card + " et se situe sur la case " + card.getLocation() + " du plateau");
+
+                if (!card.isRecurrent()) {
+                    card.getOperation().apply(this, player);
+                }
+
+                if (old != null) {
+                    log(Log.State.GREEN, "\t" + player + Log.State.GREEN + " a chasser " + old);
+                    log(Log.State.RED, "\t" + "Faveur divine lancée pour " + old);
+                    List<Facet> facetUp = getInventory(old).throwDices();
+                    log(Log.State.GREEN, "\t" + old + Log.State.GREEN + " a lancé " + facetUp);
+                    facetUp.forEach(facet -> facet.getOperation().apply(this, old));
+                }
             }
         }
     }
@@ -153,31 +176,32 @@ public class Game {
         return cardSanctuary;
     }
 
-
-    public Set<Player> getPlayers() {
-        return players.keySet();
+    List<Player> getPlayers() {
+        return new ArrayList<>(players.keySet());
     }
 
-    public Player getPlayer(String name) {
+    Player getPlayer(int number) {
         return players.keySet().stream()
-            .filter(t -> t.getName().equals(name))
+            .filter(t -> t.getId() == number)
             .findFirst().orElse(null);
     }
 
-    /** prend un player en parametre et retourne l'inventaire associé au joueur
-     *
-     * */
-
+    /**
+     * prend un player en parametre et retourne l'inventaire associé au joueur
+     */
     public Inventory getInventory(Player player) {
         return players.get(player);
     }
 
-    /** cette fonction renvoie les gagnants
+    public HashMap<Integer, Inventory> getInventories() {
+        return new HashMap<>(players.entrySet().stream().collect(Collectors.toMap((Map.Entry<Player, Inventory> e) -> e.getKey().getId(), Map.Entry::getValue)));
+    }
+
+    /**
+     * cette fonction renvoie les gagnants
      *
      * @return une liste contenant les gagnants
-     *
-     * */
-
+     */
     List<Player> getBestPlayer() {
         if (players.size() == 0) {
             return new ArrayList<>();
@@ -195,41 +219,10 @@ public class Game {
             .collect(Collectors.toList());
     }
 
-    /** Function pour Hammergold , permet de rajouter du gold sur la carte de deplacement de cette carte
-     * @param player le joueur qui effectue l'action
-     * @param gold le nombre de gold
-     * */
-
-    void addGold(Player player, int gold){
-        Inventory inventory = getInventory(player);
-        if (inventory.getActiveHammerCardCount() > 0 && inventory.getMaxRessources(ResourceType.GOLD) > inventory.getResource(ResourceType.GOLD)) {
-            Tuple<Integer, Integer> repartition = player.chooseGoldRepartion(inventory, gold);
-            if (repartition.getY() + repartition.getX() <= gold) {
-                inventory.addResources(ResourceType.GOLD, repartition.getX());
-                inventory.addGoldHammer(repartition.getY());
-                log(Log.State.ACTION,player + " a mis " + repartition.getX() + " gold dans son inventaire et " + repartition.getY() + " gold dans son marteau");
-            }
-        } else if (inventory.getMaxRessources(ResourceType.GOLD) <= inventory.getResource(ResourceType.GOLD) && inventory.getActiveHammerCardCount() > 0) {
-            inventory.addGoldHammer(gold);
-            log(Log.State.ACTION,player + " a mis " + gold + " gold dans son marteau");
-        } else {
-            inventory.addResources(ResourceType.GOLD, gold);
-            log(Log.State.ACTION,player + " a mis " + gold + " gold dans son inventaire");
-        }
-    }
-
-
-
-    //TODO P2 : GAME STATE HISTORY
-
-    private String toString(Player player) {
-        return player.toString() + "\n" + players.get(player).toString();
-    }
-
     @Override
     public String toString() {
-        return getPlayers().stream().map(player -> player + "\n" + getInventory(player) + "\n\tLocalisation :\t" + cardSanctuary.getPlayerLocation(player)).collect(Collectors.joining("\n"))
-            + "\n" + Log.State.fmt(Log.State.STATUS, "État du sanctuaire de dés") + "\n" + diceSanctuary
-            + "\n" + Log.State.fmt(Log.State.STATUS, "État du sanctuaire de cartes") + "\n" + cardSanctuary;
+        return getPlayers().stream().map(player -> player.toFmtString() + "\n" + Log.State.fmt(Log.State.GREEN, getInventory(player) + "\n\tLocalisation\t:\t" + cardSanctuary.getPlayerLocation(player))).collect(Collectors.joining("\n"))
+            + "\n" + Log.State.fmt(Log.State.BLUE, "État du sanctuaire de dés") + "\n" + diceSanctuary
+            + "\n" + Log.State.fmt(Log.State.BLUE, "État du sanctuaire de cartes") + "\n" + cardSanctuary;
     }
 }
