75bc3b1e0e80a46cbee5e18e630e296af09a31a0
tao <zhaopeng.tao@etu.unice.fr>
2018-12-15 15:59:17 +0100

    #47

--- src/main/java/com/github/l3pi/game/Player.java
+++ src/main/java/com/github/l3pi/game/Player.java
@@ -1,6 +1,6 @@
 package com.github.l3pi.game;
 
-import com.github.l3pi.type.CardLocationType;
+import com.github.l3pi.sys.Log;
 import com.github.l3pi.type.ResourceType;
 import com.github.l3pi.utilities.Tuple;
 
@@ -10,45 +10,47 @@ import java.util.List;
  * Class tasked with handling "what a player is", including his behaviour.
  * <p>
  * Base interface-type class for every AI that may be built.
- *
+ * <p>
  * classe abstraite pour représenté un joueur
- * @param name c'est le nom du joueur
  */
 public abstract class Player implements Comparable<Player> {
 
-    /**@param p c'est le joueur a comparer avec ce joueur lui meme , on compare les noms
-     *
-     *
-     * */
+    private int id;
+
+    public Player(int number) {
+        this.id = number;
+    }
+
+    /**
+     * @param p c'est le joueur a comparer avec ce joueur lui meme , on compare les noms
+     */
     @Override
     public int compareTo(Player p) {
-        return getName().compareTo(p.getName());
+        return Integer.compare(getId(), p.getId());
     }
 
-    private String name;
-
-    public Player(String name) {
-        this.name = name;
-    }
-
-    /** retourne le nom du joueur
-     *
-     * */
+    /**
+     * retourne le nom du joueur
+     */
 
     public String getName() {
-        return this.name;
+        return this.getClass().getSimpleName() + "#" + id;
     }
 
-    /** ce sont les méthodes a implementer pour chaque instance de cette classe
-     *
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * ce sont les méthodes a implementer pour chaque instance de cette classe
+     * <p>
      * chooseFacetToForge la facon de choisir une face de dé
      * chooseCard la facon de choisir une carte
      * tradeGoldForGlory action pour echanger type de ressource
      * chooseResource choisir le type de ressource pour acheter , changer dans l'inventaire
      * chooseDice choisir une des dé
-     * forgeMyDice le joueur demande a l'inventaire d'invoquer cette methode pour forger une face de dé
-     *
-     * */
+     * forgeDice le joueur demande a l'inventaire d'invoquer cette methode pour forger une face de dé
+     */
 
     public abstract Facet chooseFacetToForge(List<Facet> facetList, Game game);
 
@@ -64,13 +66,18 @@ public abstract class Player implements Comparable<Player> {
 
     public abstract int chooseDice(List<Dice> dices);
 
-    public abstract int[] forgeMyDice(Game game,Facet facet);
+    public abstract int[] forgeDice(Game game, Facet facet);
 
-    public abstract Tuple<Integer,Integer> chooseGoldRepartion(Inventory inventory, int gold);
+    public abstract Tuple<Integer, Integer> chooseGoldRepartion(Inventory inventory, int gold);
+
+    public String toFmtString() {
+        return Log.State.fmt(Log.State.BLUE, toString());
+    }
+
+    public abstract boolean moreAction(Game game);
 
     @Override
     public String toString() {
-
-        return getName() + " (" + getClass().getSimpleName() + ")";
+        return Log.State.BLUE + getName();
     }
 }
