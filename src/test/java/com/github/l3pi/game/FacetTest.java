73f730297569d828f5dc2b6a95639bc2a5105f25
Rohydre <sushi.soumrany@gmail.com>
2018-12-15 15:35:05 +0100

    WIP : Updated facet test according to new facet

--- src/test/java/com/github/l3pi/game/FacetTest.java
+++ src/test/java/com/github/l3pi/game/FacetTest.java
@@ -1,30 +1,48 @@
 package com.github.l3pi.game;
-import com.github.l3pi.type.ResourceType;
-import com.github.l3pi.utilities.Tuple;
+
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
-import java.util.List;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 
 public class FacetTest {
 
-    private Facet facet;
+    private Facet facet1;
+    private Facet facet2;
+
+    private Operation op1;
+    private Operation op2;
 
     @Before
     public void setUp(){
-        this.facet = new Facet("1 Gold", ((Game game, Player player) -> {
-            game.getInventory(player).addResources(ResourceType.GOLD, 1);
-        }));
+        op1 = (game, player) -> {
+        };
+        op2 = (game, player) -> {
+        };
 
+        facet1 = new Facet("facet1", 4, op1);
+        facet2 = new Facet("facet2", op2);
     }
 
     @Test
-    public void testMethod(){
-        assertTrue(facet.getName() == "1 Gold");
+    public void getOperation() {
+        assertEquals("facet1", facet1.getName());
+        assertEquals("facet2", facet2.getName());
+    }
 
-        Operation op = this.facet.getOperation();
-        assertEquals(op,this.facet.getOperation());
+    @Test
+    public void getName() {
+        assertEquals(op1, facet1.getOperation());
+        assertNotEquals(op2, facet1.getOperation());
+        assertEquals(op2, facet2.getOperation());
+        assertNotEquals(op1, facet2.getOperation());
+    }
+
+    @Test
+    public void getGoldCost() {
+        assertEquals(4, facet1.getGoldCost());
+        assertEquals(0, facet2.getGoldCost());
     }
 }
