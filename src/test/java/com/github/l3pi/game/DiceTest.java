61a48bc2de68ea14f518f7dfd19d884b6f929499
William Poitevin <william.poitevin@gmail.com>
2018-12-15 16:33:48 +0100

    Test Dice End #44

--- src/test/java/com/github/l3pi/game/DiceTest.java
+++ src/test/java/com/github/l3pi/game/DiceTest.java
@@ -1,35 +1,79 @@
 package com.github.l3pi.game;
 
-import com.github.l3pi.type.ResourceType;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
 import java.util.ArrayList;
 
+import static org.junit.Assert.*;
+
 public class DiceTest {
+
+    private Dice dice;
+    private ArrayList<Facet> facets;
 
     @Before
     public void setUp(){
 
+        facets = new ArrayList<Facet>() {{
+            add(new Facet("facet1", (game, player) -> {
+            }));
+            add(new Facet("facet2", (game, player) -> {
+            }));
+            add(new Facet("facet3", (game, player) -> {
+            }));
+            add(new Facet("facet4", (game, player) -> {
+            }));
+            add(new Facet("facet5", (game, player) -> {
+            }));
+            add(new Facet("facet6", (game, player) -> {
+            }));
+        }};
+
+        dice = new Dice(facets);
     }
 
     @Test
     public void throwDice() {
+        Facet result = dice.throwDice();
+        assertEquals(dice.getFaceUp(), result);
     }
 
     @Test
     public void getFaceUp() {
+        assertEquals(dice.getFacet(0), dice.getFaceUp());
     }
 
     @Test
     public void forgeFacet() {
+        Facet facet = new Facet("facet7", (game, player) -> {});
+        Facet oldFacet = dice.getFacet(5);
+
+        dice.forgeFacet(facet, 5);
+
+        assertEquals(facet, dice.getFaceUp());
+        assertFalse(dice.getFacets().stream().anyMatch(facet1 -> facet1.equals(oldFacet)));
     }
 
     @Test
     public void getFacet() {
+        for (int i = 0; i < facets.size(); i++) {
+            assertEquals(facets.get(i), dice.getFacet(i));
+        }
+
+        boolean th = false;
+
+        try {
+            dice.getFacet(7);
+        } catch (IndexOutOfBoundsException e) {
+            th = true;
+        }
+
+        assertTrue(th);
     }
 
     @Test
     public void getFacets() {
+        assertArrayEquals(facets.toArray(), dice.getFacets().toArray());
     }
 }
